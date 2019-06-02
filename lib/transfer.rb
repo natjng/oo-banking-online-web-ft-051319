3e0a0f274a819bec488e2b6fbd73ed506e47d1d9
Natalie J <natsetc@gmail.com>
2019-06-02 07:58:23 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -35,28 +35,28 @@ class Transfer
   end
 end
 
-  def execute_transaction
-    if valid? && sender.balance > amount && self.status == "pending"
-      sender.balance -= amount
-      receiver.balance += amount
-      self.status = "complete"
-    else
-      reject_transfer
-    end
-  end
+#   def execute_transaction
+#     if valid? && sender.balance > amount && self.status == "pending"
+#       sender.balance -= amount
+#       receiver.balance += amount
+#       self.status = "complete"
+#     else
+#       reject_transfer
+#     end
+#   end
 
-  def reverse_transfer
-    if valid? && receiver.balance > amount && self.status == "complete"
-      receiver.balance -= amount
-      sender.balance += amount
-      self.status = "reversed"
-    else
-      reject_transfer
-    end
-  end
+#   def reverse_transfer
+#     if valid? && receiver.balance > amount && self.status == "complete"
+#       receiver.balance -= amount
+#       sender.balance += amount
+#       self.status = "reversed"
+#     else
+#       reject_transfer
+#     end
+#   end
 
-  def reject_transfer
-    self.status = "rejected"
-    "Transaction rejected. Please check your account balance."
-  end
-end
+#   def reject_transfer
+#     self.status = "rejected"
+#     "Transaction rejected. Please check your account balance."
+#   end
+# end
